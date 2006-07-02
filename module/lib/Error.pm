720bb8fdd4404135319a0634f3752ffc73bf5429
leonerd <leonerd@52c325ad-5fd0-0310-8a0f-c43feede02cc>
2006-07-02 13:23:15 +0000

    Added :warndie import tag, which sets up __WARN__ and __DIE__ message handlers

--- module/lib/Error.pm
+++ module/lib/Error.pm
@@ -8,6 +8,8 @@
 # <peter@weblogic.com> and adapted by Jesse Glick <jglick@sig.bsh.com>.
 #
 # but modified ***significantly***
+#
+# Error::WarnDie added by Paul Evams <leonerd@leonerd.org.uk>
 
 package Error;
 
@@ -47,8 +49,20 @@ use Scalar::Util ();
 
 sub import {
     shift;
+    my @tags = @_;
     local $Exporter::ExportLevel = $Exporter::ExportLevel + 1;
-    Error::subs->import(@_);
+    
+    @tags = grep { 
+       if( $_ eq ':warndie' ) {
+          Error::WarnDie->import();
+          0;
+       }
+       else {
+          1;
+       }
+    } @tags;
+
+    Error::subs->import(@tags);
 }
 
 # I really want to use last for the name of this method, but it is a keyword
@@ -500,6 +514,93 @@ sub otherwise (&;$) {
 }
 
 1;
+
+package Error::WarnDie;
+
+sub gen_callstack($)
+{
+   my ( $start ) = @_;
+
+   require Carp;
+   local $Carp::CarpLevel = $start;
+   my $trace = Carp::longmess("");
+   # Remove try calls from the trace
+   $trace =~ s/(\n\s+\S+__ANON__[^\n]+)?\n\s+eval[^\n]+\n\s+Error::subs::try[^\n]+(?=\n)//sog;
+   $trace =~ s/(\n\s+\S+__ANON__[^\n]+)?\n\s+eval[^\n]+\n\s+Error::subs::run_clauses[^\n]+\n\s+Error::subs::try[^\n]+(?=\n)//sog;
+   my @callstack = split( m/\n/, $trace );
+   return @callstack;
+}
+
+sub DEATH
+{
+   my ( $e ) = @_;
+
+   die @_ if $^S;
+
+   my ( $etype, $message, $location, @callstack );
+   if ( ref($e) && $e->isa( "Error" ) ) {
+      $etype = "exception of type " . ref( $e );
+      $message = $e->text;
+      $location = $e->file . ":" . $e->line;
+      @callstack = split( m/\n/, $e->stacktrace );
+   }
+   else {
+      # Don't apply subsequent layer of message formatting
+      die $e if( $e =~ m/^\nUnhandled perl error caught at toplevel:\n\n/ );
+      $etype = "perl error";
+      my $stackdepth = 0;
+      while( caller( $stackdepth ) =~ m/^Error(?:$|::)/ ) {
+         $stackdepth++
+      }
+
+      @callstack = gen_callstack( $stackdepth + 1 );
+
+      $message = "$e";
+      chomp $message;
+
+      if ( $message =~ s/ at (.*?) line (\d+)\.$// ) {
+         $location = $1 . ":" . $2;
+      }
+      else {
+         my @caller = caller( $stackdepth );
+         $location = $caller[1] . ":" . $caller[2];
+      }
+   }
+
+   shift @callstack;
+   # Do it this way in case there are no elements; we don't print a spurious \n
+   my $callstack = join( "", map { "$_\n"} @callstack );
+
+   die "\nUnhandled $etype caught at toplevel:\n\n  $message\n\nThrown from: $location\n\nFull stack trace:\n\n$callstack\n";
+}
+
+sub TAXES
+{
+   my ( $message ) = @_;
+
+   $message =~ s/ at .*? line \d+\.$//;
+   chomp $message;
+
+   my @callstack = gen_callstack( 1 );
+   my $location = shift @callstack;
+
+   # $location already starts in a leading space
+   $message .= $location;
+
+   # Do it this way in case there are no elements; we don't print a spurious \n
+   my $callstack = join( "", map { "$_\n"} @callstack );
+
+   warn "$message:\n$callstack";
+}
+
+sub import
+{
+   $SIG{__DIE__}  = \&DEATH;
+   $SIG{__WARN__} = \&TAXES;
+}
+
+1;
+
 __END__
 
 =head1 NAME
@@ -798,6 +899,17 @@ class MyError::Bar by default:
         # Error handling here.
     }
 
+=cut
+
+=head1 MESSAGE HANDLERS
+
+C<Error> also provides handlers to extend the output of the C<warn()> perl
+function, and to handle the printing of a thrown C<Error> that is not caught
+or otherwise handled. These are not installed by default, but are requested
+using the C<:warndie> tag in the C<use> line.
+
+=cut
+
 =head1 KNOWN BUGS
 
 None, but that does not mean there are not any.
@@ -819,3 +931,4 @@ Shlomi Fish <shlomif@iglu.org.il>
 Arun Kumar U <u_arunkumar@yahoo.com>
 
 =cut
+
---
+++ module/t/08warndie.t
@@ -0,0 +1,157 @@
+#!/usr/bin/perl -w
+
+use strict;
+
+use Test::More tests => 13;
+
+use Error qw/ :warndie /;
+
+# Turn on full stack trace capture
+$Error::Debug = 1;
+
+# Returns the line number it is called from
+sub this_line()
+{
+   my @caller = caller();
+   return $caller[2];
+}
+
+# This file's name - for string matching
+my $file = $0;
+
+# Most of these tests are fatal, and print data on STDERR. We therefore use
+# this testing function to run a CODEref in a child process and captures its
+# STDERR and exit code.
+my ( $s, $exitcode );
+my $linekid = this_line + 16; # the $code->() is 16 lines below this one
+sub run_kid(&)
+{
+   my ( $code ) = @_;
+
+   my $kid = open( my $childh, "-|" );
+      
+   defined $kid or
+      die "Can't pipe/fork myself - $!";
+
+   if ( !$kid ) {
+      close STDERR;
+      open STDERR, ">&", STDOUT;
+
+      $! = 3; # This number should be returned from a die
+
+      $code->();
+
+      exit( 5 ); # This number should be returned if the $code block returns
+   }
+
+   $s = "";
+   while( defined ( $_ = <$childh> ) ) {
+      $s .= $_;
+   }
+
+   $exitcode = 0;
+   unless( close( $childh ) ) {
+      warn "Error closing pipe - $!" if $!;
+      $exitcode = ($? >> 8) & 0xff;
+   }   
+}
+
+ok(1, "Loaded");
+
+run_kid {
+   print STDERR "Print to STDERR\n";
+};
+
+is( $s, "Print to STDERR\n", "Test framework STDERR" );
+is( $exitcode, 5, "Test framework exitcode" );
+
+my $line;
+
+$line = this_line;
+run_kid {
+   warn "A warning\n";
+};
+
+my ( $linea, $lineb ) = ( $line + 2, $line + 3 );
+like( $s, qr/^A warning at $file line $linea:
+\tmain::__ANON__\(\) called at $file line $linekid
+\tmain::run_kid\('CODE\(0x[0-9a-f]+\)'\) called at $file line $lineb
+$/, "warn \\n-terminated STDERR" );
+is( $exitcode, 5, "warn \\n-terminated exit code" );
+
+$line = this_line;
+run_kid {
+   warn "A warning";
+};
+
+( $linea, $lineb ) = ( $line + 2, $line + 3 );
+like( $s, qr/^A warning at $file line $linea:
+\tmain::__ANON__\(\) called at $file line $linekid
+\tmain::run_kid\('CODE\(0x[0-9a-f]+\)'\) called at $file line $lineb
+$/, "warn unterminated STDERR" );
+is( $exitcode, 5, "warn unterminated exit code" );
+
+$line = this_line;
+run_kid {
+   die "An error\n";
+};
+
+( $linea, $lineb ) = ( $line + 2, $line + 3 );
+like( $s, qr/^
+Unhandled perl error caught at toplevel:
+
+  An error
+
+Thrown from: $file:$linea
+
+Full stack trace:
+
+\tmain::__ANON__\(\) called at $file line $linekid
+\tmain::run_kid\('CODE\(0x[0-9a-f]+\)'\) called at $file line $lineb
+
+$/, "die \\n-terminated STDERR" );
+is( $exitcode, 3, "die \\n-terminated exit code" );
+
+$line = this_line;
+run_kid {
+   die "An error";
+};
+
+( $linea, $lineb ) = ( $line + 2, $line + 3 );
+like( $s, qr/^
+Unhandled perl error caught at toplevel:
+
+  An error
+
+Thrown from: $file:$linea
+
+Full stack trace:
+
+\tmain::__ANON__\(\) called at $file line $linekid
+\tmain::run_kid\('CODE\(0x[0-9a-f]+\)'\) called at $file line $lineb
+
+$/, "die unterminated STDERR" );
+is( $exitcode, 3, "die unterminated exit code" );
+
+$line = this_line;
+run_kid {
+   throw Error( -text => "An exception" );
+};
+
+( $linea, $lineb ) = ( $line + 2, $line + 3 );
+like( $s, qr/^
+Unhandled exception of type Error caught at toplevel:
+
+  An exception
+
+Thrown from: $file:$linea
+
+Full stack trace:
+
+\tmain::__ANON__\(\) called at $file line $linekid
+\tmain::run_kid\('CODE\(0x[0-9a-f]+\)'\) called at $file line $lineb
+
+$/, "Error STDOUT" );
+is( $exitcode, 3, "Error exit code" );
+
+# Done
